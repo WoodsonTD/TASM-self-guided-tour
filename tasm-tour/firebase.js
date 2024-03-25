a39f6bd384d48847abc89a75f705b09363e6f21a
hayes28 <heather.hayes918@gmail.com>
2024-03-25 14:11:17 -0500

    Added to gitignore

--- tasm-tour/firebase.js
+++ tasm-tour/firebase.js
@@ -3,8 +3,7 @@ import 'firebase/analytics';
 // import 'firebase/hosting';
 // import 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const { initializeApp, getAnalytics } = firebase;
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
