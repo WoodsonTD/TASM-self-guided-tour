1dcb416fda8413d90227c1114788742e8f42b3ae
Lindsey Lancaster <104876852+linszay@users.noreply.github.com>
2024-03-26 12:18:15 -0500

    updated styling for form (#14)

--- tasm-staff-editor/src/components/exhibit_form.js
+++ tasm-staff-editor/src/components/exhibit_form.js
@@ -5,45 +5,59 @@ class ExhibitForm extends React.Component {
         return (
         <div>
             <h1>Exhibit Form</h1>
-            <form>
+            <form className="mt-2 flex-column justify-center rounded-lg px-6 py-10">
                 <label>
                     Title:
-                    <input type="text" name="title" />
+                    <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
-                <label>
-                    Media Type:
-                    <select name='mediaType'>
-                        <option value='image'>Image</option>
-                        <option value='video'>Video</option>
-                        <option value='audio'>Audio</option>
-                        <option value='text'>None</option>
-                    </select>
-                    <input type="list" name="mediaType" />
-                </label>
+                <div>
+                    <label>
+                        ID:
+                        <input type="text" name="id" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    </label>
+                    <label>
+                        Media Type:
+                        <select name='mediaType' defaultValue='None' className='text-darkBlue block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:max-w-xs sm:text-sm sm:leading-6'>
+                            <option value='image'>Image</option>
+                            <option value='video'>Video</option>
+                            <option value='audio'>Audio</option>
+                            <option value='text'>None</option>
+                        </select>
+                    </label>
+                </div>
+                <br></br>
                 <label>
                     Media Link:
-                    <input type="text" name="mediaLink" />
+                    <input type="text" name="mediaLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Audio Link:
-                    <input type="text" name="audioLink" />
+                    <input type="text" name="audioLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     QR Link:
-                    <input type="text" name="qrLink" />
+                    <input type="text" name="qrLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Content:
-                    <input type="text" name="content" />
                 </label>
-                <textarea>Additional reading</textarea>
+                <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+                </div>
+                <p className='text-center'>Additional reading</p>
                 <label>
                     Link:
-                    <input type="text" name="articleLink" />
+                    <input type="text" name="articleLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Link:
-                    <input type="text" name="articleLink" />
+                    <input type="text" name="articleLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <button type="submit" name='new'>New</button>
                 <button type="submit" name='submit'>Submit</button>
