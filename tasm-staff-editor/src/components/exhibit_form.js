62196dc1065b72e7c3416a1bc07a43886d3aee8f
Lindsey Lancaster <104876852+linszay@users.noreply.github.com>
2024-03-27 12:20:05 -0500

    updated styling, state management (#21)
    * updated styling for form
    
    * added styling to correct background issue with it not fitting to screen size - need to fix react app body size to correct issue
    
    * Add form functionality to ExhibitForm component
    
    added state management, and form submission (needs to be connected to firebase)
    
    * Update CSS class names in App.js
    
    fixed issue of missing -
    
    * updated dependencies

--- tasm-staff-editor/src/components/exhibit_form.js
+++ tasm-staff-editor/src/components/exhibit_form.js
@@ -1,23 +1,54 @@
 import React from 'react'
 
 class ExhibitForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            id: '',
+            mediaType: 'None',
+            mediaLink: '',
+            audioLink: '',
+            qrLink: '',
+            content: '',
+            articleLink: ['']
+        };
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    };
+
+    handleAddArticleLink = () => {
+        this.setState((prevState) => ({
+            articleLink: [...prevState.articleLink, '']
+        }));
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Form submitted', this.state);
+    }
+
     render() {
+        const { title, id, mediaType, mediaLink, audioLink, qrLink, content, articleLink } = this.state;
         return (
         <div>
             <h1>Exhibit Form</h1>
-            <form className="mt-2 flex-column justify-center rounded-lg px-6 py-10">
+            <form className="mt-2 flex-column justify-center rounded-lg px-6 py-10" onSubmit={this.handleSubmit}>
                 <label>
                     Title:
-                    <input type="text" name="title" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="title" value={title} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <div>
                     <label>
                         ID:
-                        <input type="text" name="id" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                        <input type="text" name="id" value={id} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                     </label>
                     <label>
                         Media Type:
-                        <select name='mediaType' defaultValue='None' className='text-darkBlue block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:max-w-xs sm:text-sm sm:leading-6'>
+                        <select name='mediaType' value={mediaType} onChange={this.handleChange} className='text-darkBlue block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:max-w-xs sm:text-sm sm:leading-6'>
                             <option value='image'>Image</option>
                             <option value='video'>Video</option>
                             <option value='audio'>Audio</option>
@@ -28,36 +59,38 @@ class ExhibitForm extends React.Component {
                 <br></br>
                 <label>
                     Media Link:
-                    <input type="text" name="mediaLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="mediaLink" value={mediaLink} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Audio Link:
-                    <input type="text" name="audioLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="audioLink" value={audioLink} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     QR Link:
-                    <input type="text" name="qrLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="qrLink" value={qrLink} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Content:
                 </label>
                 <div className="mt-2">
                 <textarea
-                  id="about"
-                  name="about"
+                  id="content"
+                  name="content"
+                  onChange={this.handleChange}
                   rows={3}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6"
                   defaultValue={''}
+                  value={content}
                 />
                 </div>
                 <p className='text-center'>Additional reading</p>
                 <label>
                     Link:
-                    <input type="text" name="articleLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="articleLink" value={articleLink} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <label>
                     Link:
-                    <input type="text" name="articleLink" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
+                    <input type="text" name="articleLink" value={articleLink} onChange={this.handleChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:text-sm sm:leading-6" />
                 </label>
                 <button type="submit" name='new'>New</button>
                 <button type="submit" name='submit'>Submit</button>
