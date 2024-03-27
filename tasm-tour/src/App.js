dbcdbd98c88e14696ba1fff3f513bb4f8113cf14
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2024-03-27 14:01:58 -0500

    Dumby info in App for testing components

--- tasm-tour/src/App.js
+++ tasm-tour/src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Header from './components/Header/Header';
 import ExhibitPage from './components/ExhibitPage/ExhibitPage';
 import './App.css';
+import ExhibitTitle from './components/ExhibitTitle/ExhibitTitle';
+import ModelView from './components/AFrame/ModelView';
+import ButtonPanel from './components/ButtonPanel/ButtonPanel';
 
 function App() {
   return (
     <div className="bg-gray h-screen">
     <Header />
-    <ExhibitPage />
+    <ExhibitTitle title="Welcome to the TASM Tour" />
+    <ModelView modelID="voyager" sky='black'/>
+    <p> Content Text will be here!</p>
+    <ButtonPanel />
+    {/* <ExhibitPage /> */}
     </div>
   );
 }
