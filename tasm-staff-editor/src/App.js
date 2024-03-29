2fa293e0d0ff7eee30c9539aeb21fe0997a59fe2
linszay <lindseyjlancaster@gmail.com>
2024-03-29 18:52:41 -0500

    Update import paths for ExhibitForm component

--- tasm-staff-editor/src/App.js
+++ tasm-staff-editor/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import './assets/images/tasm-favicon.png';
-import './components/exhibit_form.js';
-import ExhibitForm from './components/exhibit_form.js';
+import './components/ExhibitForm/exhibit_form.js';
+import ExhibitForm from './components/ExhibitForm/exhibit_form.js';
 import Nav from './components/nav.js';
 
 function App() {
