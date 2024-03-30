b82c7bf79801db97e4ee73078ded35b011200097
linszay <lindseyjlancaster@gmail.com>
2024-03-29 19:40:51 -0500

    Fix import statement for ExhibitForm component after updating file name

--- tasm-staff-editor/src/App.js
+++ tasm-staff-editor/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import './assets/images/tasm-favicon.png';
-import './components/ExhibitForm/exhibit_form.js';
-import ExhibitForm from './components/ExhibitForm/exhibit_form.js';
+import ExhibitForm from './components/ExhibitForm/ExhibitForm.js';
 import Nav from './components/nav.js';
 
 function App() {
