3ce6a72895f056dbd4b57763e91923fab0fb294f
FosterClark48 <fosterclark48@yahoo.com>
2024-04-01 15:23:13 -0500

    Added for attribute and an id for accessibility

--- tasm-staff-editor/src/components/MediaType/MediaType.js
+++ tasm-staff-editor/src/components/MediaType/MediaType.js
@@ -5,10 +5,10 @@ export default function MediaType({ mediaType, mediaLink, onChange }) {
   return (
     <div className="flex flex-col space-y-4 mb-4">
       <div className="flex flex-col">
-        <label className="mr-2 text-white">
+        <label htmlFor="media-type" className="mr-2 text-white">
           Media Type:
         </label>
-        <select name='mediaType' value={mediaType} onChange={onChange} className='text-darkBlue flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600'>
+        <select name='mediaType' id="media-type" value={mediaType} onChange={onChange} className='text-darkBlue flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600'>
           <option value='image'>Image</option>
           <option value='video'>Video</option>
           <option value='text'>None</option>
@@ -16,14 +16,14 @@ export default function MediaType({ mediaType, mediaLink, onChange }) {
       </div>
       {mediaType === 'image' && (
         <div className="flex flex-col mb-4">
-          <label className="mr-2 text-white">Upload Image:</label>
-          <input type="file" name="mediaLink" onChange={onChange} className="flex-1" />
+          <label htmlFor="upload-image" className="mr-2 text-white">Upload Image:</label>
+          <input type="file" name="mediaLink" id="upload-image" onChange={onChange} className="flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6" />
         </div>
       )}
       {mediaType === 'video' && (
         <div className="flex flex-col mb-4">
-          <label className="mr-2 text-white">Add Video Link:</label>
-          <input type="text" name="mediaLink" value={mediaLink} onChange={onChange} className="flex-1" />
+          <label htmlFor="add-video-link" className="mr-2 text-white">Add Video Link:</label>
+          <input type="text" name="mediaLink" id="add-video-link" value={mediaLink} onChange={onChange} className="flex-1" />
         </div>
       )}
     </div>
