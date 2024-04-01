fbe05ff7f91aa1edf62abd3eccd29286da602954
FosterClark48 <fosterclark48@yahoo.com>
2024-04-01 14:34:15 -0500

    fixed label and and input div

--- tasm-staff-editor/src/components/ExhibitTitle/ExhibitTitle.js
+++ tasm-staff-editor/src/components/ExhibitTitle/ExhibitTitle.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 export default function ExhibitTitle({ title, onChange }) {
   return (
-    <label className='block mb-4'>
-      Title:
+    <div>
+      <label className='block mb-1 text-white'>
+        Title:
+      </label>
       <input
         type="text"
         name="title"
@@ -11,6 +13,6 @@ export default function ExhibitTitle({ title, onChange }) {
         onChange={onChange}
         className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
       />
-    </label>
+    </div>
   );
 };
