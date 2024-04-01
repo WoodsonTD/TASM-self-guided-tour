e89f4ab45e872d27b8734490c7cde5f619506526
WoodsonTD <taylordwoodson@gmail.com>
2024-04-01 17:57:50 -0500

    put pack store

--- tasm-staff-editor/src/firebase.js
+++ tasm-staff-editor/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export { app, analytics, db, storage };
