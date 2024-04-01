36d2c03ae2b0a2deeed1d91988f2675cc4dbc1c4
FosterClark48 <fosterclark48@yahoo.com>
2024-04-01 14:35:33 -0500

    Added button component. made text white

--- tasm-staff-editor/src/components/ReadingLinks/ReadingLinks.js
+++ tasm-staff-editor/src/components/ReadingLinks/ReadingLinks.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Button from '../ButtonPanel/Button.js';
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 export default function ReadingLinks({ articleLink = [], onChange, onAddArticleLink }) {
   return (
     <div>
-      <p className='text-center text-xl block mb-4'>Additional reading</p>
+      <p className='text-center text-white text-xl block mb-4'>Additional reading</p>
       {articleLink.map((link, index) => (
-        <label key={index} className='block mb-4'>
-          Link:
+        <div>
+          <label key={index} className='block mb-1 text-white'>
+            Link:
+          </label>
           <input
             type="text"
             name={`articleLink[${index}]`}
@@ -14,11 +18,16 @@ export default function ReadingLinks({ articleLink = [], onChange, onAddArticleL
             onChange={(event) => onChange(event, index)}
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
           />
-        </label>
+        </div>
       ))}
-      <button type="button" onClick={onAddArticleLink}>
-        Add Link
-      </button>
+      <Button
+        label="Add Link"
+        onClick={() => console.log('Add Link clicked')}
+        icon={PlusIcon}
+        iconProps={{ className: "w-7 h-7" }}
+        iconPosition="left"
+        className="btn rounded-full pl-3 pr-4 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+      />
     </div>
   );
 }
