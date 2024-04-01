ccb379064dba74d88642e59b455d5749788931fb
FosterClark48 <fosterclark48@yahoo.com>
2024-04-01 14:32:46 -0500

    make component a flexbox. fixed default media type. need to style more still

--- tasm-staff-editor/src/components/MediaType/MediaType.js
+++ tasm-staff-editor/src/components/MediaType/MediaType.js
@@ -3,26 +3,28 @@ import React from 'react';
 export default function MediaType({ mediaType, mediaLink, onChange }) {
 
   return (
-    <div>
-      <label>
-        Media Type:
-        <select name='mediaType' value={mediaType} onChange={onChange} className='text-darkBlue block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:max-w-xs sm:leading-6'>
+    <div className="flex flex-col space-y-4 mb-4">
+      <div className="flex flex-col">
+        <label className="mr-2 text-white">
+          Media Type:
+        </label>
+        <select name='mediaType' value={mediaType} onChange={onChange} className='text-darkBlue flex-1 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600'>
           <option value='image'>Image</option>
           <option value='video'>Video</option>
           <option value='text'>None</option>
         </select>
-      </label>
-      <br></br>
-      {mediaType !== 'text' && (
-        <label className='block mb-4'>
-          {mediaType === 'image' ? 'Upload Image' : 'Add Video Link'}
-          {mediaType === 'image' && (
-            <input type="file" name="mediaLink" onChange={onChange} className="upload-image-button block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6" />
-          )}
-          {mediaType === 'video' && (
-            <input type="text" name="mediaLink" value={mediaLink} onChange={onChange} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6" />
-          )}
-        </label>
+      </div>
+      {mediaType === 'image' && (
+        <div className="flex flex-col mb-4">
+          <label className="mr-2 text-white">Upload Image:</label>
+          <input type="file" name="mediaLink" onChange={onChange} className="flex-1" />
+        </div>
+      )}
+      {mediaType === 'video' && (
+        <div className="flex flex-col mb-4">
+          <label className="mr-2 text-white">Add Video Link:</label>
+          <input type="text" name="mediaLink" value={mediaLink} onChange={onChange} className="flex-1" />
+        </div>
       )}
     </div>
   );
