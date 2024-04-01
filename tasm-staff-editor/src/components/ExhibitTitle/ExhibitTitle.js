b726b7c48df3ddfade69bfb4404e4414ee41d03a
Heather Hayes <107968573+hayes28@users.noreply.github.com>
2024-04-01 10:14:23 -0500

    Firestore is setup and linked, qr is linked to data. Still working on… (#38)
    * Firestore is setup and linked, qr is linked to data. Still working on all of it.
    
    * Okay I got it to now link the data entered into firestore, it is not perfect but a good start

--- tasm-staff-editor/src/components/ExhibitTitle/ExhibitTitle.js
+++ tasm-staff-editor/src/components/ExhibitTitle/ExhibitTitle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ExhibitTitle({ title, handleChange }) {
+export default function ExhibitTitle({ title, onChange }) {
     return (
         <label className='block mb-4'>
             Title:
@@ -8,7 +8,7 @@ export default function ExhibitTitle({ title, handleChange }) {
                 type="text"
                 name="title"
                 value={title}
-                onChange={handleChange}
+                onChange={onChange}
                 className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
             />
         </label>
