a6238d829c54dac3a3647a0b7587e378316f82d9
FosterClark48 <fosterclark48@yahoo.com>
2024-04-02 11:31:16 -0500

    Updated Additional Reading section to 'Further Reading' for consistency. Added handleLinkChange function to take in an object with title and link. Added extra input to add title. made input able to type in them.

--- tasm-staff-editor/src/components/ReadingLinks/ReadingLinks.js
+++ tasm-staff-editor/src/components/ReadingLinks/ReadingLinks.js
@@ -3,21 +3,37 @@ import Button from '../ButtonPanel/Button.js';
 import { PlusIcon } from '@heroicons/react/24/outline';
 
 export default function ReadingLinks({ articleLink = [], onChange, onAddArticleLink }) {
+  const handleLinkChange = (event, index, key) => {
+    const newLinks = [...articleLink];
+    newLinks[index] = { ...newLinks[index], [key]: event.target.value };
+    onChange({ target: { name: 'articleLink', value: newLinks } });
+  };
+
   return (
     <div>
-      <p className='text-center text-white text-xl block mb-4'>Additional reading</p>
+      <p className='text-center text-white text-xl block mb-4'>Further Reading</p>
       {articleLink.map((link, index) => (
         <div key={index}>
-          <label htmlFor="link" className='block mb-1 text-white'>
+          <label htmlFor={`further-title-${index}`} className='block mb-1 text-white'>
+            Title:
+          </label>
+          <input
+            type="text"
+            id={`further-title-${index}`}
+            placeholder="Link Title"
+            value={link.title}
+            onChange={(event) => handleLinkChange(event, index, 'title')}
+            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
+          />
+          <label htmlFor={`further-link-${index}`} className='block mb-1 text-white'>
             Link:
           </label>
           <input
             type="text"
-            name={`articleLink[${index}]`}
-            id="link"
-            value={link}
+            id={`further-link-${index}`}
+            value={link.link}
             placeholder='Add link here'
-            onChange={(event) => onChange(event, index)}
+            onChange={(event) => handleLinkChange(event, index, 'link')}
             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
           />
         </div>
