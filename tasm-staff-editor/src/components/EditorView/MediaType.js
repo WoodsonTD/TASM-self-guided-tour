ce0870cf8e6423542ce581863a29f7b2f3636184
FosterClark48 <fosterclark48@yahoo.com>
2024-04-02 15:31:48 -0500

    formatted element attributes for readability

--- tasm-staff-editor/src/components/EditorView/MediaType.js
+++ tasm-staff-editor/src/components/EditorView/MediaType.js
@@ -23,7 +23,13 @@ export default function MediaType({ mediaType, mediaLink, onChange }) {
         <label htmlFor="media-type" className="mr-2 text-white">
           Media Type:
         </label>
-        <select name='mediaType' id="media-type" value={mediaType} onChange={onChange} className='text-darkBlue rounded-md mt-1 border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red'>
+        <select
+          name='mediaType'
+          id="media-type"
+          value={mediaType}
+          onChange={onChange}
+          className='text-darkBlue rounded-md mt-1 border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-red'
+        >
           <option value='image'>Image</option>
           <option value='video'>Video</option>
           <option value='text'>None</option>
@@ -32,13 +38,25 @@ export default function MediaType({ mediaType, mediaLink, onChange }) {
       {mediaType === 'image' && (
         <div className="flex flex-col mb-4">
           <label className="mr-2 text-white">Upload Image:</label>
-          <input type="file" name="mediaLink" accept='image/*' onChange={handleImageUpload} className="mt-1 text-white" />
+          <input
+            type="file"
+            name="mediaLink"
+            accept='image/*'
+            onChange={handleImageUpload}
+            className="mt-1 text-white"
+          />
         </div>
       )}
       {mediaType === 'video' && (
         <div className="flex flex-col mb-4">
           <label className="mr-2 text-white">Add Video Link:</label>
-          <input type="text" name="mediaLink" value={mediaLink} onChange={onChange} className="mt-1 rounded-md" />
+          <input
+            type="text"
+            name="mediaLink"
+            value={mediaLink}
+            onChange={onChange}
+            className="mt-1 rounded-md"
+          />
         </div>
       )}
     </div>
