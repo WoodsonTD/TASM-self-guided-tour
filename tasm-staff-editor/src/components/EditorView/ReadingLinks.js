7ad05dd0cfa0e5f66d6d0d1af288bdadb22dd87a
foster clark <105602291+FosterClark48@users.noreply.github.com>
2024-04-02 15:33:07 -0500

    A lot of styling updates to Editor View. File organization as well (#62)
    * updated articleLink useState to start with an object w/ title and link. Updated handleChange function to use the value from the event. updated handleAddArticleLink function to store object w/ title and link
    
    * Changed 4-Digit Code text ONLY (not prop) to 'Exhibit ID'
    
    * Updated Additional Reading section to 'Further Reading' for consistency. Added handleLinkChange function to take in an object with title and link. Added extra input to add title. made input able to type in them.
    
    * commented out Login Form
    
    * Updated imports after file reorganization. Added styling to bg to fill page.
    
    * Transformed class component into functional component. Put in it's own directory
    
    * Updated file organization to hold all editor view items in an EditorView directory. Updated styling to format as a flexbox
    
    * updated spacing between label and input
    
    * Updated some styling and spacing between labels, select, and input elements
    
    * moved into ExhibitView directory. changed p tag text from '4-digit Code' to 'exhibitID'
    
    * Moved into ExhibitView directory. Added margin between input and label
    
    * Moved to exhbitView directory and updated imports. Updated styles to use flexbox (need to style further)
    
    * imported useEffect from react. destructured user from userCredential on line 11.
    
    * removed ExhibitID component because it wasn't being used anywhere. organized everything else into the ExhibitView directory
    
    * formatted element attributes for readability

---
+++ tasm-staff-editor/src/components/EditorView/ReadingLinks.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import Button from '../ButtonPanel/Button.js';
+import { PlusIcon } from '@heroicons/react/24/outline';
+
+export default function ReadingLinks({ articleLink = [], onChange, onAddArticleLink }) {
+  const handleLinkChange = (event, index, key) => {
+    const newLinks = [...articleLink];
+    newLinks[index] = { ...newLinks[index], [key]: event.target.value };
+    onChange({ target: { name: 'articleLink', value: newLinks } });
+  };
+
+  return (
+    <div>
+      <p className='text-center text-white text-xl block mb-4'>Further Reading</p>
+      {articleLink.map((link, index) => (
+        <div key={index}>
+          <label htmlFor={`further-title-${index}`} className='block mb-1 text-white'>
+            Title:
+          </label>
+          <input
+            type="text"
+            id={`further-title-${index}`}
+            placeholder="Link Title"
+            value={link.title}
+            onChange={(event) => handleLinkChange(event, index, 'title')}
+            className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
+          />
+          <label htmlFor={`further-link-${index}`} className='block mb-1 text-white'>
+            Link:
+          </label>
+          <input
+            type="text"
+            id={`further-link-${index}`}
+            value={link.link}
+            placeholder='Add link here'
+            onChange={(event) => handleLinkChange(event, index, 'link')}
+            className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
+          />
+        </div>
+      ))}
+      <div className="mt-4">
+        <Button
+          label="Add Link"
+          onClick={onAddArticleLink}
+          icon={PlusIcon}
+          iconProps={{ className: "w-7 h-7" }}
+          iconPosition="left"
+          className="btn rounded-full pl-3 pr-4 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+        />
+      </div>
+    </div>
+  );
+}
