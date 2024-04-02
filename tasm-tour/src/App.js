32c181a1c114a3f944f8673ff46255ca0cffd791
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2024-04-02 13:28:19 -0500

    Remove unused console log

--- tasm-tour/src/App.js
+++ tasm-tour/src/App.js
@@ -8,7 +8,6 @@ function App() {
   const searchParams = new URLSearchParams(window.location.search);
   const [exhibitID, setExhibitID] = useState(searchParams.get('exhibitID') || 'test');
 
-  console.log(exhibitID);
   return (
     <div className="bg-gray ld-h-screen bg-h-screen font-exo2">
       <Header exhibitID={exhibitID} setExhibitID={setExhibitID} />
