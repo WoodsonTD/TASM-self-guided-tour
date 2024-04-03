0a8f7da7d06669f294af40f0264661b9e0653db7
linszay <lindseyjlancaster@gmail.com>
2024-04-03 12:58:05 -0500

    Refactor ListViewComponents to fetch exhibit data from Firebase
    current exhibits display exhibit name and title

--- tasm-staff-editor/src/components/ListViewComponents/ListViewComponents.js
+++ tasm-staff-editor/src/components/ListViewComponents/ListViewComponents.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import { db } from '../../firebase.js'
+import { collection, getDocs } from 'firebase/firestore'
+import { useState, useEffect } from 'react';
+import ListViewItem from './ListViewItem.js';
 
-export default function ListViewComponents() {
-    // Sample exhibit data
-    const exhibitData = [
-        { name: 'Exhibit 1', id: 1 },
-        { name: 'Exhibit 2', id: 2 },
-        { name: 'Exhibit 3', id: 3 },
-        // Add more exhibit data as needed
-    ];
+export default function ListViewComponents({ entry, setEntry }) {
 
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Exhibit Name</th>
-                        <th>Exhibit ID</th>
-                        <th>EDIT</th>
-                        <th>HIDE</th>
-                        <th>ORDER</th>
-                        <th>Move Up</th>
-                        <th>Move Down</th>
-                        <th>Delete Exhibit Page</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {exhibitData.map((exhibit) => (
-                        // need to add the rest of the code for thissss
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+  const [exhibitData, setExhibitData] = useState([]);
+  // let exhibitData = [];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const q = await getDocs(collection(db, 'exhibits'));
+        setExhibitData(q.docs);
+      } catch (error) {
+        console.error("ERROR:"+error);
+      }
+    }
+    fetchData();
+  }, [entry]);
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Exhibit Name</th>
+            <th>Exhibit ID</th>
+            <th>EDIT</th>
+            <th>HIDE</th>
+            <th>ORDER</th>
+            <th>Move Up</th>
+            <th>Move Down</th>
+            <th>Delete Exhibit Page</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exhibitData.map((exhibit) => <ListViewItem exhibit={exhibit} />)}
+        </tbody>
+      </table>
+    </div>
+  );
 };
