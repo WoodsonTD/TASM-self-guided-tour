66f9b38840361bf669e76f19304f59183efca536
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2024-04-03 11:02:36 -0500

    Pass in Next and Prev to button pannel

--- tasm-tour/src/components/ButtonPanel/ButtonPanel.js
+++ tasm-tour/src/components/ButtonPanel/ButtonPanel.js
@@ -1,25 +1,29 @@
 import Button from './Button';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/outline';
 
-export default function ButtonPanel({ setExhibitID, nextExhibit = "", prevExhibit = "" }) {
+export default function ButtonPanel({ setExhibitID, nextExhibit, prevExhibit }) {
   return (
     <div className="flex justify-around p-4">
-      <Button
-        label="Prev"
-        onClick={() => setExhibitID((prevExhibit))}
-        icon={ChevronLeftIcon}
-        iconProps={{ className: "w-7 h-7" }}
-        iconPosition="left"
-        className="btn rounded-l-full pl-1 pr-3 py-1 text-xl drop-shadow-[-2px_3px_4px_rgba(0,0,0,0.25)]"
-      />
-      <Button
-        label="Next"
-        onClick={() => setExhibitID(nextExhibit || 'test')}
-        icon={ChevronRightIcon}
-        iconProps={{ className: "w-7 h-7" }}
-        iconPosition="right"
-        className="btn rounded-r-full pr-1 pl-3 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
-      />
+      {prevExhibit ?
+        <Button
+          label="Prev"
+          onClick={() => setExhibitID(prevExhibit)}
+          icon={ChevronLeftIcon}
+          iconProps={{ className: "w-7 h-7" }}
+          iconPosition="left"
+          className="btn rounded-l-full pl-1 pr-3 py-1 text-xl drop-shadow-[-2px_3px_4px_rgba(0,0,0,0.25)]"
+        />
+        : null}
+      {nextExhibit ?
+        <Button
+          label="Next"
+          onClick={() => setExhibitID(nextExhibit)}
+          icon={ChevronRightIcon}
+          iconProps={{ className: "w-7 h-7" }}
+          iconPosition="right"
+          className="btn rounded-r-full pr-1 pl-3 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+        />
+        : null}
     </div>
   );
 }
