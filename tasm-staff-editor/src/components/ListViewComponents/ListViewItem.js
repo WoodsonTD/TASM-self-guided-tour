68a33f839957722fd55971f1e188288416ca8fee
linszay <lindseyjlancaster@gmail.com>
2024-04-03 15:03:08 -0500

    Edit button is connected and mostly functional. It needs a bit more work to get the exhibit page selected to open in the editor view.
    Add base delete button.
    handleDelete func needs to be finished to be functional delete button.

--- tasm-staff-editor/src/components/ListViewComponents/ListViewItem.js
+++ tasm-staff-editor/src/components/ListViewComponents/ListViewItem.js
@@ -1,24 +1,42 @@
 import Button from "../ButtonPanel/Button";
-export default function ListViewItem({ exhibit }) {
+export default function ListViewItem({ exhibit, setEntry }) {
   if (!exhibit) {
     console.error("ERROR: exhibit is null");
     return null;
   }
+  const handleDelete = () => {
+    const confirmDelete = window.confirm("Are you sure you want to delete this exhibit?");
+    if (confirmDelete) {
+      // Perform the delete operation here
+      // Still needs to be added
+      setEntry(exhibit.id);
+    }
+  };
+
   return (
     <tr key={exhibit.id}>
       <td>{exhibit.data().title}</td>
       <td>{exhibit.data().fourDigitCode}</td>
-      <td><Button
-        label="EDIT"
-        onClick={() => console.log('Next clicked')}
-        icon={null}
-        className="btn rounded-r-full pr-1 pl-3 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
-      /></td>
+      <td>
+        <Button
+          label="EDIT"
+          onClick={() => setEntry(exhibit.id)}
+          icon={null}
+          className="btn rounded-md py-0.5 px-3 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+        />
+      </td>
       <td>HIDE</td>
       <td>ORDER</td>
       <td>Move Up</td>
       <td>Move Down</td>
-      <td>Delete Exhibit Page</td>
+      <td>
+        <Button
+          label="DELETE"
+          onClick={handleDelete}
+          icon={null}
+          className="btn rounded-md py-0.5 px-3 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+        />
+      </td>
     </tr>
   );
 }
