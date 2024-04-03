ccc9fe17943a0ae1a7e1dcded5c23ed56ede071c
FosterClark48 <fosterclark48@yahoo.com>
2024-04-03 12:45:05 -0500

    Created Apply directive for Nav Bar

--- tasm-staff-editor/src/components/NavBar/nav.js
+++ tasm-staff-editor/src/components/NavBar/nav.js
@@ -4,7 +4,7 @@ function Nav() {
 
   return (
     <div>
-      <span className="bg-darkRed text-white py-2 px-4 inline-block rounded-br-lg">
+      <span className="nav">
         <a href='https://console.firebase.google.com/u/1/project/tasm-tour/analytics/app/web:NTQ5NzcyYjMtMTkyYS00NTJhLWEwNGItNGVlMzgwMGNmZDdi/overview/reports~2Fdashboard%3Fr%3Dfirebase-overview&fpn%3D602869699809'>Firebase Analytics Link</a>
       </span>
     </div>
