344dd634b1ab0ec4ab777f9c522e1963534db295
FosterClark48 <fosterclark48@yahoo.com>
2024-04-04 11:38:22 -0500

    Added input styling

--- tasm-staff-editor/src/components/EditorView/ExhibitTitle.js
+++ tasm-staff-editor/src/components/EditorView/ExhibitTitle.js
@@ -10,10 +10,11 @@ export default function ExhibitTitle({ title, onChange }) {
         <input
           type="text"
           name="title"
+          placeholder="Exhibit Title"
           id="title"
           value={title}
           onChange={onChange}
-          className="w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
+          className="input"
         />
       </div>
     </div>
