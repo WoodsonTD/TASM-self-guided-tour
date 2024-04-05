94f5aaadeede10e92e18bd4861b318bcb9b9933f
FosterClark48 <fosterclark48@yahoo.com>
2024-04-05 16:35:15 -0500

    Added prop for text visibility

--- tasm-staff-editor/src/components/ButtonPanel/Button.js
+++ tasm-staff-editor/src/components/ButtonPanel/Button.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function Button({ label, className, onClick, icon: Icon, iconProps = {}, iconPosition = 'left', type = 'button', ariaLabel }) {
+function Button({ label, className, onClick, icon: Icon, iconProps = {}, iconPosition = 'left', type = 'button', ariaLabel, textVisibilityClass = '' }) {
   return (
     <button className={`${className}`} onClick={onClick} type={type} aria-label={ariaLabel || label}>
       {iconPosition === 'left' && Icon && <Icon {...iconProps} />}
-      {label && <span>{label}</span>}
+      {label && <span className={`${textVisibilityClass}`}>{label}</span>}
       {iconPosition === 'right' && Icon && <Icon {...iconProps} />}
     </button>
   );
