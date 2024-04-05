b74e0485ade45be32b11ebbd08be4ce9d663719b
Philip Taylor <Oggal@users.noreply.github.com>
2024-04-05 12:14:24 -0500

    ListView navigation improvements (#73)
    * Hide the 'Hide' button
    
    * Setup Nav Back Button
    
    * Implement Back Warning
    
    * Update ListViewComponents and ListViewItem styles
    
    * word wrap correction for user view
    
    * Small Style changes
    
    * Fix QR not linking to ExhibitID
    
    cleaned up some whitespace and comments as well
    
    * remove incomplete style

--- tasm-staff-editor/src/components/ListViewComponents/ListViewComponents.js
+++ tasm-staff-editor/src/components/ListViewComponents/ListViewComponents.js
@@ -1,7 +1,8 @@
 import { db } from '../../firebase.js'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, getDocs, addDoc } from 'firebase/firestore'
 import { useState, useEffect } from 'react';
 import ListViewItem from './ListViewItem.js';
+import Button from '../ButtonPanel/Button.js';
 
 export default function ListViewComponent({ entry, setEntry }) {
 
@@ -20,15 +21,28 @@ export default function ListViewComponent({ entry, setEntry }) {
     fetchData();
   }, [entry]);
 
+  const handleAddExhibit = async () => {
+    const docRef = await addDoc(collection(db, 'exhibits'), {});
+      console.log('Exhibit data saved to Firestore');
+      setEntry(docRef.id);
+  }
+
   return (
     <div>
-      <table>
+      <h1 className='text-2xl text-white text-center'>Exhibit List</h1>
+      <Button
+        label="Add New Exhibit"
+        onClick={() => handleAddExhibit()}
+        icon={null}
+        className="btn rounded-md py-0.5 px-3 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+      />
+      <table className='mx-auto text-left rounded-md bg-lightBlue bg-opacity-20 border-collapse'>
         <thead>
           <tr>
             <th>Exhibit Name</th>
             <th>Exhibit ID</th>
             <th>EDIT</th>
-            <th>HIDE</th>
+            {/* <th>HIDE</th> */}
             <th>ORDER</th>
             <th>Move Up</th>
             <th>Move Down</th>
