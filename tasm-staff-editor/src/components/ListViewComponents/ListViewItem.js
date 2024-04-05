7ed5fcf2b65c5240880faa6da6aa4f41e4a8a093
foster clark <105602291+FosterClark48@users.noreply.github.com>
2024-04-05 16:37:29 -0500

    Updated styling for list view, nav, index.css (#78)
    * updated articleLink useState to start with an object w/ title and link. Updated handleChange function to use the value from the event. updated handleAddArticleLink function to store object w/ title and link
    
    * Changed 4-Digit Code text ONLY (not prop) to 'Exhibit ID'
    
    * Updated Additional Reading section to 'Further Reading' for consistency. Added handleLinkChange function to take in an object with title and link. Added extra input to add title. made input able to type in them.
    
    * commented out Login Form
    
    * Updated imports after file reorganization. Added styling to bg to fill page.
    
    * Transformed class component into functional component. Put in it's own directory
    
    * Updated file organization to hold all editor view items in an EditorView directory. Updated styling to format as a flexbox
    
    * updated spacing between label and input
    
    * Updated some styling and spacing between labels, select, and input elements
    
    * moved into ExhibitView directory. changed p tag text from '4-digit Code' to 'exhibitID'
    
    * Moved into ExhibitView directory. Added margin between input and label
    
    * Moved to exhbitView directory and updated imports. Updated styles to use flexbox (need to style further)
    
    * imported useEffect from react. destructured user from userCredential on line 11.
    
    * removed ExhibitID component because it wasn't being used anywhere. organized everything else into the ExhibitView directory
    
    * formatted element attributes for readability
    
    * Added styling to nav link
    
    * Created Apply directive for Nav Bar
    
    * Added a custom lightRed color
    
    * Added custom classes/apply directives for buttons and inputs
    
    * more styling on the text area
    
    * Added input styling
    
    * Added quite a bit of styling to image file upload, and overall style to component
    
    * Added styling for components labels and inputs and button
    
    * Added custom notAsDarkRed color for button hover effect
    
    * Added active effects for button and file button
    
    * updated padding for list view
    
    * Added prop for text visibility
    
    * Added div and updated styling
    
    * updated styling
    
    * updated styling

--- tasm-staff-editor/src/components/ListViewComponents/ListViewItem.js
+++ tasm-staff-editor/src/components/ListViewComponents/ListViewItem.js
@@ -1,4 +1,6 @@
 import Button from "../ButtonPanel/Button";
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+
 export default function ListViewItem({ exhibit, setEntry }) {
   if (!exhibit) {
     console.error("ERROR: exhibit is null");
@@ -14,28 +16,62 @@ export default function ListViewItem({ exhibit, setEntry }) {
   };
 
   return (
-    <tr className="my-20 border-collapse border-t-2 border-opacity-50 border-darkBlue" key={exhibit.id}>
-      <td className="bg-opacity-15 bg-lightBlue  rounded-l-xl">{exhibit.data().title}</td>
-      <td className="bg-opacity-15 bg-lightBlue">{exhibit.data().exhibitID}</td>
+    <tr className="border-t-8 border-opacity-0 border-darkBlue" key={exhibit.id}>
+      <td className="max-w-0 md:w-auto md:max-w-none whitespace-nowrap bg-opacity-15 bg-lightBlue rounded-l-xl">
+        {exhibit.data().title}
+      </td>
+      <td className="text-ellipsis text-center md:text-left bg-opacity-15 bg-lightBlue">
+        {exhibit.data().exhibitID}
+      </td>
       <td className="bg-opacity-15 bg-lightBlue">
-        <Button
-          label="EDIT"
-          onClick={() => setEntry(exhibit.id)}
-          icon={null}
-          className="btn rounded-md py-0.5 px-3 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
-        />
+        <div className="flex justify-center">
+          <Button
+            label="EDIT"
+            onClick={() => setEntry(exhibit.id)}
+            icon={null}
+            className="btn rounded-md py-0.5 px-3 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+          />
+        </div>
       </td>
       {/* <td>HIDE</td> */}
-      <td className="bg-opacity-15 bg-lightBlue">ORDER</td>
-      <td className="bg-opacity-15 bg-lightBlue">Move Up</td>
-      <td className="bg-opacity-15 bg-lightBlue">Move Down</td>
-      <td className="bg-opacity-15 bg-lightBlue rounded-r-xl">
-        <Button
-          label="DELETE"
-          onClick={handleDelete}
-          icon={null}
-          className="btn rounded-md py-0.5 px-3 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)] mx-auto mr-1"
-        />
+      <td className="hidden md:table-cell bg-opacity-15 bg-lightBlue">
+        ORDER
+      </td>
+      <td className="bg-opacity-15 bg-lightBlue">
+        <div className="flex justify-center">
+          <Button
+            label="MOVE"
+            onClick={null}
+            icon={ChevronUpIcon}
+            iconProps={{ className: "w-6 h-6 md:w-5 md:h-5 ml-0 md:ml-1" }}
+            iconPosition="right"
+            textVisibilityClass="hidden md:inline"
+            className="btn rounded-md py-0.5 px-2 md:pl-3 md:pr-2 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+          />
+        </div>
+      </td>
+      <td className="bg-opacity-15 bg-lightBlue">
+        <div className="flex justify-center">
+          <Button
+            label="MOVE"
+            onClick={null}
+            icon={ChevronDownIcon}
+            iconProps={{ className: "w-6 h-6 md:w-5 md:h-5 ml-0 md:ml-1" }}
+            iconPosition="right"
+            textVisibilityClass="hidden md:inline"
+            className="btn rounded-md py-0.5 px-2 md:pl-3 md:pr-2 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+          />
+        </div>
+      </td>
+      <td className="hidden sm:table-cell bg-opacity-15 bg-lightBlue rounded-r-xl">
+        <div className="flex justify-center">
+          <Button
+            label="DELETE"
+            onClick={handleDelete}
+            icon={null}
+            className="btn rounded-md py-0.5 px-3 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+          />
+        </div>
       </td>
     </tr>
   );
--- tasm-staff-editor/src/components/NavBar/nav.js
+++ tasm-staff-editor/src/components/NavBar/nav.js
@@ -11,22 +11,21 @@ function Nav({selected, setSelected}) {
   }
 
   return (
-    <div className='flex flex-row-reverse justify-between'>
-      
+    <div className='flex flex-row-reverse justify-between mb-3'>
       <span className=''>
         <a
-          className="nav py-2 px-4" 
+          className="nav py-2 px-4"
           href='https://console.firebase.google.com/u/1/project/tasm-tour/analytics/app/web:NTQ5NzcyYjMtMTkyYS00NTJhLWEwNGItNGVlMzgwMGNmZDdi/overview/reports~2Fdashboard%3Fr%3Dfirebase-overview&fpn%3D602869699809'>Firebase Analytics Link</a>
       </span>
       {selected ?
         <span className="">
-          <Button 
+          <Button
           onClick={handleBack}
           label="Back"
           icon={ChevronDoubleLeftIcon}
           iconProps={{className: "w-5 h-5"}}
           iconPosition="left"
-          className={"btn_nav py-2 px-4"} 
+          className={"btn_nav py-2 px-4"}
           />
         </span>
         : null
