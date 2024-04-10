d48e3170763bac4f1734df7f8b7297463af2eed3
FosterClark48 <fosterclark48@yahoo.com>
2024-04-10 15:28:41 -0500

    styled signUp correctly

--- tasm-staff-editor/src/components/SignUp/SignUp.js
+++ tasm-staff-editor/src/components/SignUp/SignUp.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Button from "../ButtonPanel/Button";
 
 const SignUp = ({ onClose }) => {
   const [formData, setFormData] = useState({
@@ -22,94 +23,89 @@ const SignUp = ({ onClose }) => {
   };
 
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div className="fixed inset-0 transition-opacity" aria-hidden="true">
-          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <h2 className="text-lg font-medium text-gray-900">Sign Up</h2>
-            <form className="mt-4" onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                  htmlFor="firstName"
-                >
-                  First Name
-                </label>
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
-                  id="firstName"
-                  type="text"
-                  placeholder="First Name"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                  htmlFor="lastName"
-                >
-                  Last Name
-                </label>
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
-                  id="lastName"
-                  type="text"
-                  placeholder="Last Name"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="mb-4">
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                  htmlFor="email"
-                >
-                  Email
-                </label>
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
-                  id="email"
-                  type="email"
-                  placeholder="Email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-                <input
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-lightBlue-600 sm:leading-6"
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                />
-              </div>
-              <div className="flex justify-end">
-                <button
-                  type="submit"
-                  className="btn rounded-r-full pr-1 pl-3 py-1 text-xl drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
-                >
-                  Sign Up
-                </button>
-              </div>
-            </form>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-full max-w-md">
+        <form
+          className="bg-gray shadow-md rounded-lg px-8 py-6 mb-4 drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="text-3xl text-black text-center mb-4">Sign Up</h1>
+          <div className="mb-4">
+            <label
+              className="block mb-2 text-lg text-black font-medium"
+              htmlFor="firstName"
+            >
+              First Name
+            </label>
+            <input
+              className="input"
+              id="firstName"
+              type="text"
+              placeholder="First Name"
+              value={formData.firstName}
+              onChange={handleChange}
+              required
+            />
           </div>
-        </div>
+          <div className="mb-4">
+            <label
+              className="block mb-2 text-lg text-black font-medium"
+              htmlFor="lastName"
+            >
+              Last Name
+            </label>
+            <input
+              className="input"
+              id="lastName"
+              type="text"
+              placeholder="Last Name"
+              value={formData.lastName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block mb-2 text-lg text-black font-medium"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="input"
+              id="email"
+              type="email"
+              placeholder="Email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block mb-2 text-lg text-black font-medium"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="input"
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="flex justify-end">
+            <Button
+              label="Sign Up"
+              type="submit"
+              className="btn rounded-xl p-2 text-lg drop-shadow-[2px_3px_4px_rgba(0,0,0,0.25)]"
+            />
+          </div>
+        </form>
       </div>
     </div>
   );
