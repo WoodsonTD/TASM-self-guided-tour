e730fbad0d0848667434963028145dfee4994a81
WoodsonTD <taylordwoodson@gmail.com>
2024-04-10 12:07:05 -0500

    imported signup

--- tasm-staff-editor/src/App.js
+++ tasm-staff-editor/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import './assets/images/tasm-favicon.png';
 import SignIn from './components/SignIn/SignIn.js';
 import Editor from './components/Editor/Editor.js';
+import SignUp from './components/SignUp/SignUp.js';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
 
   const handleCloseSignIn = () => {
     setIsLoggedIn(true);
+  };
+
+  const handleToggleSignUp = () => {
+    setShowSignUp(!showSignUp);
   };
 
   return (
@@ -16,8 +22,14 @@ function App() {
       {isLoggedIn ? (
         <Editor />
       ) : (
-        <SignIn onClose={handleCloseSignIn} />
-  )}
+        <>
+          {showSignUp ? (
+            <SignUp onClose={handleToggleSignUp} />
+          ) : (
+            <SignIn onClose={handleCloseSignIn} onSignUpClick={handleToggleSignUp} />
+          )}
+          </>
+      )}
     </div>
   );
 }
