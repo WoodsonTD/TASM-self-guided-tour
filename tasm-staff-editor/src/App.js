78940e32becc42c8277da0103e4fa45416bc4c46
FosterClark48 <fosterclark48@yahoo.com>
2024-05-02 17:56:06 -0500

    removed SignUp component since it lives in Editor file now

--- tasm-staff-editor/src/App.js
+++ tasm-staff-editor/src/App.js
@@ -3,28 +3,20 @@ import './App.css';
 import './assets/images/tasm-favicon.png';
 import SignIn from './components/SignIn/SignIn.js';
 import Editor from './components/Editor/Editor.js';
-import SignUp from './components/SignUp/SignUp.js';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [activeForm, setActiveForm] = useState('signIn');
 
   const handleCloseSignIn = () => {
     setIsLoggedIn(true);
-  };
-
-  const handleToggleForm = () => {
-    setActiveForm(prevForm => prevForm === 'signIn' ? 'signUp' : 'signIn');
   };
 
   return (
     <div className="App bg-TASM-bg min-h-screen min-w-screen bg-cover bg-left-bottom bg-no-repeat font-exo2">
       {isLoggedIn ? (
         <Editor />
-      ) : activeForm === 'signUp' ? (
-        <SignUp onClose={handleToggleForm} onSignInClick={handleToggleForm} />
       ) : (
-        <SignIn onClose={handleCloseSignIn} onSignUpClick={handleToggleForm} />
+        <SignIn onClose={handleCloseSignIn} />
       )}
     </div>
   );
